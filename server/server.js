1f3439c678f6dd158ee55d3ea1bb3eb5c333112a
AimenDev <aimendev4@gmail.com>
2024-10-29 18:10:22 +0100

    deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connect.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,16 +8,25 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
+dotenv.config();
+
 app.use(express.json());
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
